9f5e22c56918979917f71ed071375fd0c78e7a09
psyjb12 <psyjb12@nottingham.ac.uk>
2019-06-24 14:31:40 +0100

    Reading lines from CSV

--- src/Tennis/Main.kt
+++ src/Tennis/Main.kt
@@ -1,9 +1,31 @@
 package Tennis
+
+import com.opencsv.CSVReader
+import java.nio.charset.StandardCharsets
+import java.nio.file.Files
+import java.nio.file.Paths
+
 // Download rankings .csv from:
 // https://datahub.io/sports-data/atp-world-tour-tennis-data
 // include that in the Data package.
 
 fun main(args : Array<String>) {
-    println("Base")
+    readPlayersFromCSV()
+}
+
+fun readPlayersFromCSV(){
+    val filePath = Paths.get("C:\\Users\\psyjb12.AD.000\\IdeaProjects\\TennisScrape\\src\\Tennis\\Data\\rankings_1973-2017_csv.csv")
+    val reader = Files.newBufferedReader(filePath, StandardCharsets.UTF_8)
+    val csvReader = CSVReader(reader)
+
+    var line = csvReader.readNext()
+    val seenYears : List<String>
+    var numberOfSeenYears = -1
+
+    do {
+        line.forEach { print("$it | ") }
+        print("\n\n")
+        line = csvReader.readNext()
+    } while (line != null)
 
 }
